2e443335ccbe76d0e2d7d456ac99f5477848da30
Matthew1994 <wb-lmh190042@alibaba-inc.com>
2016-06-04 19:53:42 +0800

    修改导航、背景颜色，调整按钮位置

--- build/js/manage.js
+++ build/js/manage.js
@@ -136,8 +136,8 @@ $(function () {
         //showTags: true,
         levels: 3,
         //multiSelect: true,
-        expandIcon: "glyphicon glyphicon-stop",
-        collapseIcon: "glyphicon glyphicon-unchecked",
+        //expandIcon: "glyphicon glyphicon-stop",
+        //collapseIcon: "glyphicon glyphicon-unchecked",
         nodeIcon: "glyphicon glyphicon-user",
         /*
         color: "yellow",
